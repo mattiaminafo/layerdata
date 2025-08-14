3401f8c76159483a1b37b53fc1a8e5c442281836
mattiaminafo <mattia@mattiaminafo.io>
2025-08-15 00:48:50 +0200

    fix: optimize clients scrolling for mobile devices - Reduce logo container widths on mobile (w-32) for better space utilization - Make logo sizes responsive (w-24 on mobile, w-36 on desktop) - Adjust spacing between logos for mobile optimization - Ensure all 9 client logos scroll completely on mobile devices - Fix issue where scrolling stopped at 'Loop' on mobile

--- components/clients.js
+++ components/clients.js
@@ -7,155 +7,155 @@ export default function Clients() {
       
       {/* Scrolling logos container */}
       <div className="w-full overflow-hidden">
-        <div className="flex animate-scroll space-x-16 md:space-x-20">
+        <div className="flex animate-scroll space-x-8 sm:space-x-12 md:space-x-16 lg:space-x-20">
           {/* First set of logos */}
-          <div className="flex space-x-16 md:space-x-20 flex-shrink-0">
+          <div className="flex space-x-8 sm:space-x-12 md:space-x-16 lg:space-x-20 flex-shrink-0">
             {/* Boolean */}
-            <div className="flex flex-col items-center w-48">
+            <div className="flex flex-col items-center w-32 sm:w-40 md:w-48">
               <img 
                 src="/images/boolean-logo.png" 
                 alt="Boolean" 
-                className="w-36 h-36 object-contain"
+                className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 object-contain"
               />
             </div>
             {/* Loop */}
-            <div className="flex flex-col items-center w-48">
+            <div className="flex flex-col items-center w-32 sm:w-40 md:w-48">
               <img 
                 src="/images/loop-logo.png" 
                 alt="Loop" 
-                className="w-36 h-36 object-contain"
+                className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 object-contain"
               />
             </div>
             {/* L22 */}
-            <div className="flex flex-col items-center w-48">
+            <div className="flex flex-col items-center w-32 sm:w-40 md:w-48">
               <img 
                 src="/images/l22-logo.png" 
                 alt="L22" 
-                className="w-36 h-36 object-contain"
+                className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 object-contain"
               />
             </div>
             {/* Fler */}
-            <div className="flex flex-col items-center w-48">
+            <div className="flex flex-col items-center w-32 sm:w-40 md:w-48">
               <img 
                 src="/images/fler-logo.png" 
                 alt="Fler" 
-                className="w-36 h-36 object-contain"
+                className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 object-contain"
               />
             </div>
             {/* OneDay */}
-            <div className="flex flex-col items-center w-48">
+            <div className="flex flex-col items-center w-32 sm:w-40 md:w-48">
               <img 
                 src="/images/oneday-logo.webp" 
                 alt="OneDay" 
-                className="w-36 h-36 object-contain"
+                className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 object-contain"
               />
             </div>
             {/* UTRAVEL */}
-            <div className="flex flex-col items-center w-48">
+            <div className="flex flex-col items-center w-32 sm:w-40 md:w-48">
               <img 
                 src="/images/utravel-logo.png" 
                 alt="UTRAVEL" 
-                className="w-36 h-36 object-contain"
+                className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 object-contain"
               />
             </div>
             {/* Rougj */}
-            <div className="flex flex-col items-center w-48">
+            <div className="flex flex-col items-center w-32 sm:w-40 md:w-48">
               <img 
                 src="/images/rougj-logo.png" 
                 alt="Rougj" 
-                className="w-36 h-36 object-contain"
+                className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 object-contain"
               />
             </div>
             {/* Giusti */}
-            <div className="flex flex-col items-center w-48">
+            <div className="flex flex-col items-center w-32 sm:w-40 md:w-48">
               <img 
                 src="/images/giusti - logo.webp" 
                 alt="Giusti" 
-                className="w-36 h-36 object-contain"
+                className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 object-contain"
               />
             </div>
             {/* 24 */}
-            <div className="flex flex-col items-center w-64">
+            <div className="flex flex-col items-center w-32 sm:w-40 md:w-64">
               <img 
                 src="/images/24-logo.png" 
                 alt="24" 
-                className="w-36 h-36 object-contain"
+                className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 object-contain"
               />
             </div>
           </div>
           
           {/* Duplicate set for seamless loop */}
-          <div className="flex space-x-16 md:space-x-20 flex-shrink-0">
+          <div className="flex space-x-8 sm:space-x-12 md:space-x-16 lg:space-x-20 flex-shrink-0">
             {/* Boolean */}
-            <div className="flex flex-col items-center w-48">
+            <div className="flex flex-col items-center w-32 sm:w-40 md:w-48">
               <img 
                 src="/images/boolean-logo.png" 
                 alt="Boolean" 
-                className="w-36 h-36 object-contain"
+                className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 object-contain"
               />
             </div>
             {/* Loop */}
-            <div className="flex flex-col items-center w-48">
+            <div className="flex flex-col items-center w-32 sm:w-40 md:w-48">
               <img 
                 src="/images/loop-logo.png" 
                 alt="Loop" 
-                className="w-36 h-36 object-contain"
+                className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 object-contain"
               />
             </div>
             {/* L22 */}
-            <div className="flex flex-col items-center w-48">
+            <div className="flex flex-col items-center w-32 sm:w-40 md:w-48">
               <img 
                 src="/images/l22-logo.png" 
                 alt="L22" 
-                className="w-36 h-36 object-contain"
+                className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 object-contain"
               />
             </div>
             {/* Fler */}
-            <div className="flex flex-col items-center w-48">
+            <div className="flex flex-col items-center w-32 sm:w-40 md:w-48">
               <img 
                 src="/images/fler-logo.png" 
                 alt="Fler" 
-                className="w-36 h-36 object-contain"
+                className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 object-contain"
               />
             </div>
             {/* OneDay */}
-            <div className="flex flex-col items-center w-48">
+            <div className="flex flex-col items-center w-32 sm:w-40 md:w-48">
               <img 
                 src="/images/oneday-logo.webp" 
                 alt="OneDay" 
-                className="w-36 h-36 object-contain"
+                className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 object-contain"
               />
             </div>
             {/* UTRAVEL */}
-            <div className="flex flex-col items-center w-48">
+            <div className="flex flex-col items-center w-32 sm:w-40 md:w-48">
               <img 
                 src="/images/utravel-logo.png" 
                 alt="UTRAVEL" 
-                className="w-36 h-36 object-contain"
+                className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 object-contain"
               />
             </div>
             {/* Rougj */}
-            <div className="flex flex-col items-center w-48">
+            <div className="flex flex-col items-center w-32 sm:w-40 md:w-48">
               <img 
                 src="/images/rougj-logo.png" 
                 alt="Rougj" 
-                className="w-36 h-36 object-contain"
+                className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 object-contain"
               />
             </div>
             {/* Giusti */}
-            <div className="flex flex-col items-center w-48">
+            <div className="flex flex-col items-center w-32 sm:w-40 md:w-48">
               <img 
                 src="/images/giusti - logo.webp" 
                 alt="Giusti" 
-                className="w-36 h-36 object-contain"
+                className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 object-contain"
               />
             </div>
             {/* 24 */}
-            <div className="flex flex-col items-center w-64">
+            <div className="flex flex-col items-center w-32 sm:w-40 md:w-64">
               <img 
                 src="/images/24-logo.png" 
                 alt="24" 
-                className="w-36 h-36 object-contain"
+                className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 object-contain"
               />
             </div>
           </div>
