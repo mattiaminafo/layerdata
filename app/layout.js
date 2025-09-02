62d4d2dcb831d9ebf76dd64223dd5d68c43a90d6
mattiaminafo <mattia@mattiaminafo.io>
2025-09-02 15:01:18 +0200

    Add Google Tag Manager integration
    - Add GTM script to head section
    - Add GTM noscript fallback to body
    - Configure with GTM ID: GTM-TM77V2XK

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,32 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="cyberpunk" className={`${poppins.className} ${gotham.variable}`}>
-      <body>{children}</body>
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TM77V2XK');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TM77V2XK"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {children}
+      </body>
     </html>
   );
 }
